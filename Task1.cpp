47727f9adfdac611daac0d4b99d9da9fecce0a77
Tejas1140 <73573613+Tejas1140@users.noreply.github.com>
2023-10-17 15:48:46 +0530

    Task1.cpp

---
+++ Task1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int minSwaps(vector<int>& arr,int n) 
+{
+    int onesCount = 0;
+    
+    for (int i = 0; i < n; i++) 
+    {
+        onesCount += arr[i];
+    }
+
+    int currentOnes = 0;
+    int minSwaps = onesCount; 
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 1) {
+            currentOnes++;
+        }
+        int currentZeros = i - currentOnes + 1;
+        int swapsNeeded = onesCount - currentOnes + currentZeros;
+        minSwaps = min(minSwaps, swapsNeeded);
+    }
+    return minSwaps;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+    }
+    bool x=is_sorted(arr.begin(), arr.end());
+    //if array of elements are already sorted return 0
+    if(x==1)
+    {
+      cout<<0<<endl;
+    }
+    else
+    {
+      int ans = minSwaps(arr,n);
+      cout << ans <<endl;
+    }
+
+    return 0;
+}
